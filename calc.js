577c6199e2d88a604b0f695caa89d8c4c3fbd88b
Zeiad Eldiasty <zeiadeldiasty72@gmail.com>
2026-02-24 21:26:59 +0200

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,18 @@
+let input =prompt("enter the first number");
+let operator =prompt("enter the operator");
+let input1 =prompt("enter the second number");
+
+if(operator==="+"){
+    console.log(input+input1);
+}
+else if(operator==="*"){
+    console.log(input*input1);
+}
+else if(operator==="-"){
+    console.log(input-input1);
+}
+else if(operator==="/"){
+    console.log(input/input1);
+}
+
+
